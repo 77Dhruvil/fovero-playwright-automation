fbb9283d22ac1f5c673224786213d6e335fa33d5
77Dhruvil <pateldhruvil2119976@gmail.com>
2026-05-20 19:40:15 +0530

    change in conftest

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from playwright.sync_api import sync_playwright
 
 from pages.login_page import LoginPage
-from pages.dashboard_page import DashboardPage
+#from pages.dashboard_page import DashboardPage
 from utils.config import Config
 
 
@@ -56,4 +56,4 @@ def admin_dashboard_login(page):
     # optional small stabilization wait
     page.wait_for_timeout(2000)
 
-    return DashboardPage(page)
+    return (page)
